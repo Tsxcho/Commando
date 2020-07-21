f05cfd24f01fe822bba778229bd1b781b9ec8db3
Tsxcho <65160578+Tsxcho@users.noreply.github.com>
2020-07-21 13:05:21 +0530

    Update unknown-command.js

--- src/commands/util/unknown-command.js
+++ src/commands/util/unknown-command.js
@@ -7,7 +7,7 @@ module.exports = class UnknownCommandCommand extends Command {
 			group: 'util',
 			memberName: 'unknown-command',
 			description: 'Displays help information for when an unknown command is used.',
-			examples: ['unknown-command kickeverybodyever'],
+			examples: ['`unknown-command kickeverybodyever`'],
 			unknown: true,
 			hidden: true
 		});
